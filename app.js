a1f70be563732ff4db594d81ad16c2ae3e64b85e
Anton <fynjn77@gmail.com>
2024-06-17 06:22:46 +0300

    git from vscode

---
+++ app.js
@@ -0,0 +1,16 @@
+/*
+const person = {
+    model: 'tesla',
+    year: 2023,
+}
+
+const json = JSON.stringify(person)//делаю объект строкой.
+// В JSON кавычки ОБЯЗАТЕЛЬНО двойные.
+console.log(json)
+
+const parsed = JSON.parse(json)//делаю строку объектом
+console.log(parsed)
+*/
+
+// ПОДКЛЮЧЕНИЕ К JSON // 6:34:30 https://www.youtube.com/watch?v=fcMcf_4PjfI
+
